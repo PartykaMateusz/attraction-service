0d3a6b5a1dc2b75d46039c89067a5caa576a1401
Mateusz Partyka <mateuszpartyka12@gmail.com>
2021-04-08 22:14:54 +0200

    unit test mapper when category is restaurant

--- src/test/java/com/mytrip/attractionservice/internal/model/mapper/AttractionMapperTest.java
+++ src/test/java/com/mytrip/attractionservice/internal/model/mapper/AttractionMapperTest.java
@@ -1,21 +1,17 @@
 package com.mytrip.attractionservice.internal.model.mapper;
 
 import com.mytrip.attractionservice.internal.feign.model.attraction.AttractionResponse;
-import com.mytrip.attractionservice.internal.feign.model.attraction.RestOkAttractionsResponse;
 import com.mytrip.attractionservice.internal.feign.model.shared.AncestorResponse;
 import com.mytrip.attractionservice.internal.feign.model.shared.SubCategoryResponse;
 import com.mytrip.attractionservice.internal.model.Location;
-import com.mytrip.attractionservice.internal.service.impl.AttractionServiceImpl;
+import com.mytrip.attractionservice.internal.model.LocationType;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 public class AttractionMapperTest {
@@ -23,6 +19,7 @@ public class AttractionMapperTest {
     private static final String CITY = "CITY";
     private static final String WARSAW = "warsaw";
     private static final String POLAND = "poland";
+    private static final String SUPER_RESTAURANT = "Super restaurant";
 
     @InjectMocks
     private AttractionMapper attractionMapper;
@@ -72,6 +69,36 @@ public class AttractionMapperTest {
         assertEquals(POLAND, actualLocation.getCountry());
     }
 
+    @Test
+    public void apply_validData_CategoryIsRestaurant(){
+        AttractionResponse attractionResponse = this.generateAttractionResponse();
+        attractionResponse.setCategory(this.generateCategoryRestaurant());
+        Location actualLocation = attractionMapper.apply(attractionResponse);
+
+        assertNotNull(actualLocation);
+
+        assertEquals(attractionResponse.getLocationId(), actualLocation.getLocationId());
+        assertNotNull(actualLocation.getLocationType());
+        assertEquals(LocationType.RESTAURANT, actualLocation.getLocationType());
+        assertEquals(attractionResponse.getLocation(), actualLocation.getLocation());
+        assertEquals(attractionResponse.getName(), actualLocation.getName());
+        assertEquals(attractionResponse.getLatitude(), actualLocation.getLatitude());
+        assertEquals(attractionResponse.getLongitude(), actualLocation.getLongitude());
+        assertEquals(attractionResponse.getWebsite(), actualLocation.getWebsite());
+        assertEquals(attractionResponse.getTimezone(), actualLocation.getTimezone());
+        assertEquals(attractionResponse.getPhone(), actualLocation.getPhone());
+        assertEquals(attractionResponse.getAddress(), actualLocation.getAddress());
+        assertEquals(WARSAW, actualLocation.getCity());
+        assertEquals(POLAND, actualLocation.getCountry());
+    }
+
+    private Map<String, String> generateCategoryRestaurant() {
+        Map<String, String> category = new HashMap<>();
+        category.put("key", "restaurant");
+        category.put("name", "Restaurant");
+        return category;
+    }
+
     private Map<String, String> generateCategoryGeographic() {
         Map<String, String> category = new HashMap<>();
         category.put("key", "geographic");
@@ -92,7 +119,7 @@ public class AttractionMapperTest {
     }
 
     private List<AncestorResponse> generateAncestors() {
-        return Arrays.asList(this.generateAncestorCity(), this.generateAncestorCountry());
+        return Arrays.asList(this.generateAncestorCity(), this.generateAncestorCountry(), this.generateAncestorRestaurant());
     }
 
     private AncestorResponse generateAncestorCity() {
@@ -100,6 +127,20 @@ public class AttractionMapperTest {
         ancestorResponse.setName(WARSAW);
         ancestorResponse.setSubcategory(this.generateCitySubcategory());
         return ancestorResponse;
+    }
+
+    private AncestorResponse generateAncestorRestaurant() {
+        AncestorResponse ancestorResponse = new AncestorResponse();
+        ancestorResponse.setName(SUPER_RESTAURANT);
+        ancestorResponse.setSubcategory(this.generateRestaurantSubcategory());
+        return ancestorResponse;
+    }
+
+    private List<SubCategoryResponse> generateRestaurantSubcategory() {
+        SubCategoryResponse subCategoryResponse = new SubCategoryResponse();
+        subCategoryResponse.setKey("restaurant");
+        subCategoryResponse.setName("super restaurant");
+        return Arrays.asList(subCategoryResponse);
     }
 
     private List<SubCategoryResponse> generateCitySubcategory() {
