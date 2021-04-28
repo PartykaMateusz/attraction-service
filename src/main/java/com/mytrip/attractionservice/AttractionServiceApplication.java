4a0e02e51e60d0654689f93cb96a718687305176
Mateusz Partyka <mateuszpartyka12@gmail.com>
2021-04-28 19:45:00 +0200

    add swagger config

--- src/main/java/com/mytrip/attractionservice/AttractionServiceApplication.java
+++ src/main/java/com/mytrip/attractionservice/AttractionServiceApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
-@EnableSwagger2
 public class AttractionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttractionServiceApplication.class, args);
 	}
+
+
 
 }
---
+++ src/main/java/com/mytrip/attractionservice/SwaggerConfig.java
@@ -0,0 +1,14 @@
+package com.mytrip.attractionservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurerAdapter {
+
+
+}
+
+
